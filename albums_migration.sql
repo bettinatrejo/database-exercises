db608a23400dd0dc8425d3de6f43b589e8ab1740
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-16 16:34:07 -0500

    changed data type in table

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,10 +5,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
  id INT unsigned NOT NULL AUTO_INCREMENT,
- artist VARCHAR (50),
- name VARCHAR (100) NOT NULL,
+ artist VARCHAR (150),
+ name VARCHAR (150) NOT NULL,
  release_date INT,
  sales DECIMAL(6,2),
- genre VARCHAR(50),
+ genre VARCHAR(150),
  PRIMARY KEY (id)
 );
