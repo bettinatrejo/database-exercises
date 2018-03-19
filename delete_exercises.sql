bd86edb6dc966bb92db3bb4d8f2c03171b134466
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-19 10:06:36 -0500

    converted select statments to delete

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,14 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE release_date > 1991;
-SELECT * FROM albums WHERE genre = 'Disco';
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+-- selectors
+-- SELECT * FROM albums WHERE release_date > 1991;
+-- SELECT * FROM albums WHERE genre = 'Disco';
+-- SELECT * FROM albums WHERE artist = 'Whitney Houston';
+
+-- delete
+DELETE * FROM albums WHERE release_date > 1991;
+DELETE * FROM albums WHERE genre = 'Disco';
+DELETE * FROM albums WHERE artist = 'Whitney Houston';
 
 
 
