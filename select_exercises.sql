4a6ff9d3001eabfcaac598f206d5d3b4d3d79f7c
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-18 19:21:17 -0500

    switched syntax on line 7

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,7 +4,7 @@ USE codeup_test_db;
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT * FROM albums WHERE artist = ' Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT * FROM albums WHERE artist = 'Nevermind';
-SELECT * FROM albums WHERE release_date = 1990 to release_date = 1999;
+SELECT * FROM albums WHERE 1989 < release_date < 2000;
 SELECT * FROM albums WHERE sales < 20;
 SELECT * FROM albums WHERE genre = 'Rock';
 
