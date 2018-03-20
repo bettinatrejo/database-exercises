dcde228b4662666eb340a73ae70512c738fa584a
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-20 09:02:29 -0500

    used delete clause to alter table

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,14 +1,14 @@
 USE codeup_test_db;
 
 -- selectors
--- SELECT * FROM albums WHERE release_date > 1991;
--- SELECT * FROM albums WHERE genre = 'Disco';
--- SELECT * FROM albums WHERE artist = 'Whitney Houston';
+-- SELECT name,release_date FROM albums WHERE release_date > 1991;
+-- SELECT name,genre FROM albums WHERE genre = 'Disco';
+-- SELECT name,artist FROM albums WHERE artist = 'Whitney Houston';
 
 -- delete
-DELETE * FROM albums WHERE release_date > 1991;
-DELETE * FROM albums WHERE genre = 'Disco';
-DELETE * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
 
 
 
