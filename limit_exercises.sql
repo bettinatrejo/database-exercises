e6572c928b11251e04ef0ce5c93f6d454de0daf5
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-20 11:39:24 -0500

    used limit and offset to get specific amounts of data

---
+++ limit_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date asc, hire_date DESC
+LIMIT 5;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date asc, hire_date DESC
+LIMIT 5 OFFSET 45;
+
