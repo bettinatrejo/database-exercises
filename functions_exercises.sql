b5acdcf36237233963918afe496bc8f809d510a6
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-20 11:38:57 -0500

    used concat and datediff to peform functions for making one field and getting a difference

---
+++ functions_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+
+#Find all employees whose last name starts with 'E'
+SELECT concat(first_name,' ',last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+# Find all employees hired in the 90s and born on Christmas
+# use datediff() to find how many days they have been working at the company
+# (Hint: You will also need to use now() or curdate())
+SELECT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date asc, hire_date DESC;
+
+SELECT datediff(now(), hire_date), first_name, last_name
+From employees
+WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date asc, hire_date DESC;
+
