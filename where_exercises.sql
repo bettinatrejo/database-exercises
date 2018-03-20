16b804d27a8c36c149b281588d05533a1b1c1eeb
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-20 09:09:12 -0500

    used where clauses to update queries

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,48 +1,48 @@
 USE employees;
 
 # Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN
-SELECT first_name, last_name
+SELECT first_name
 FROM employees
 WHERE first_name = 'Irena'
-  OR first_name = 'Vidya' OR first_name = 'Maya'
-ORDER BY first_name asc;
+      OR first_name = 'Vidya' OR first_name = 'Maya';
 
 # Find all employees with first names 'Irena', 'Vidya', or 'Maya'
-SELECT first_name, last_name
+SELECT first_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name asc, last_name asc;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT first_name, last_name
+# Add a condition to the previous query to find everybody with those names who is also male
+SELECT first_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name asc, first_name asc;
+WHERE gender = 'M'
+      AND (first_name = 'Irena'
+           OR first_name = 'Vidya' OR first_name = 'Maya');
 
 #Find all employees whose last name starts with 'E'
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no asc;
-
-# Find all employees hired in the 90s and born on Christmas
-SELECT first_name, last_name, birth_date, hire_date
-FROM employees
-WHERE birth_date LIKE '%12-25' AND hire_date LIKE '199%'
-ORDER BY birth_date asc, hire_date DESC ;
-
-
+WHERE last_name LIKE 'E%';
 
 
 # Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'
-# SELECT emp_no, first_name, last_name
-# FROM employees
-# WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-#
-# # Find all employees whose last name starts or ends with 'E'
-# SELECT emp_no, first_name, last_name
-# FROM employees
-# WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
+# Find all employees whose last name starts or ends with 'E'
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+# Find all employees hired in the 90s
+SELECT hire_date
+FROM employees
+WHERE hire_date LIKE '199%';
+
+# Find all employees born on Christmas
+SELECT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE birth_date LIKE '%12-25';
 
 
 
