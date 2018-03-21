34f4f6f343eadbb16d8975fe102cd97d609c5116
Bettina Trejo <bettina.trejo12@gmail.com>
2018-03-21 14:49:42 -0500

    applied queries for avg salary of current and historical employees

--- join_exercises.sql
+++ join_exercises.sql
@@ -54,15 +54,54 @@ ORDER BY dept_name ASC;
 
 # Bonus: Find the names of all current employees, their department name, and their current manager's name
 Select CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(m.first_name, ' ', m.last_name) AS 'Manager Name'
-FROM employees as e
-  JOIN dept_emp as de
+FROM employees AS e
+  JOIN dept_emp AS de
     ON de.emp_no = e.emp_no
-  JOIN departments as d
+  JOIN departments AS d
     ON d.dept_no = de.dept_no
-  JOIN dept_manager as dm
+  JOIN dept_manager AS dm
     ON de.dept_no = dm.dept_no
-  JOIN employees as m
+  JOIN employees AS m
     ON m.emp_no = dm.emp_no
 WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
 ORDER BY dept_name;
 
+# Bonus: Write a query to get the average salary current managers make by gender
+SELECT gender,avg(salary)
+FROM employees AS e
+  JOIN salaries AS s
+    ON s.emp_no = e.emp_no
+  JOIN dept_manager AS dm
+    ON dm.emp_no = e.emp_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+GROUP BY gender;
+
+
+# Bonus: Write a query to get the avg salary managers historically make by gender
+SELECT gender,avg(salary)
+FROM employees AS e
+  JOIN salaries AS s
+    ON s.emp_no = e.emp_no
+  JOIN dept_manager AS dm
+    ON dm.emp_no = e.emp_no
+GROUP BY gender;
+
+
+# Bonus: get avg salary for all current employees by gender
+SELECT gender,avg(salary)
+FROM employees AS e
+  JOIN salaries AS s
+    ON s.emp_no = e.emp_no
+  JOIN dept_emp AS de
+    ON de.emp_no = e.emp_no
+WHERE s.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
+GROUP BY gender;
+
+# Bonus: get avg historic salary for all employees by gender
+SELECT gender,avg(salary)
+FROM employees AS e
+  JOIN salaries AS s
+    ON s.emp_no = e.emp_no
+  JOIN dept_emp AS de
+    ON de.emp_no = e.emp_no
+GROUP BY gender;
